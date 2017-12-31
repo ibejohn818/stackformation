064a9b39c7838b4e11e4fcf112ddb2369a342607
John <john.hardy@me.com>
2017-12-31 13:41:42 -0800

    Bump version: 0.2.4 → 0.2.5

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup_requirements = [
 
 setup(
     name='jh-stackformation',
-    version='0.2.4',
+    version='0.2.5',
     description="AWS CloudFormation framework",
     long_description=readme + '\n\n' + history,
     author="John Hardy",
--- stackformation/__init__.py
+++ stackformation/__init__.py
@@ -9,7 +9,7 @@ import stackformation.utils as utils
 
 __author__ = """John Hardy"""
 __email__ = 'john@johnchardy.com'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 logger = logging.getLogger(__name__)
 
