48914114f9cad5c29119393ded859994e2c406ae
John Hardy <john.hardy@me.com>
2018-01-11 22:01:24 -0800

    Bump version: 0.2.16 → 0.2.17

--- stackformation/__init__.py
+++ stackformation/__init__.py
@@ -9,7 +9,7 @@ import stackformation.utils as utils
 
 __author__ = """John Hardy"""
 __email__ = 'john@johnchardy.com'
-__version__ = '0.2.16'
+__version__ = '0.2.17'
 
 logger = logging.getLogger(__name__)
 
