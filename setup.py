6855f28eb023239c48c48cced70791b8f5ba9fa4
= <=>
2018-12-23 14:59:32 -0800

    Bump version: 0.2.35 → 0.2.36

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup_requirements = [
 
 setup(
     name='jh-stackformation',
-    version='0.2.35',
+    version='0.2.36',
     description="AWS CloudFormation framework",
     long_description=readme + '\n\n' + history,
     author="John Hardy",
