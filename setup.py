6a37be0e1c6f80ae2ed8fc72f4be43c3269bf1e4
John Hardy <john.hardy@me.com>
2019-01-07 12:53:35 -0800

    Bump version: 0.2.36 → 0.2.37

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup_requirements = [
 
 setup(
     name='jh-stackformation',
-    version='0.2.36',
+    version='0.2.37',
     description="AWS CloudFormation framework",
     long_description=readme + '\n\n' + history,
     author="John Hardy",
